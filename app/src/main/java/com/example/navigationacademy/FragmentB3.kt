0da388043bee27c67d8ad1cd57903cb37ec75890
ketly <ketlyn.ewelyn@gmail.com>
2023-05-19 12:38:50 -0300

    add the finish method at fragmentb3

--- app/src/main/java/com/example/navigationacademy/FragmentB3.kt
+++ app/src/main/java/com/example/navigationacademy/FragmentB3.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,6 +17,8 @@ private const val ARG_PARAM2 = "param2"
  * Use the [FragmentB3.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+private var finishButton: Button? = null
 class FragmentB3 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -34,7 +37,18 @@ class FragmentB3 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_b3, container, false)
+        val view = inflater.inflate(R.layout.fragment_b3, container, false)
+
+        finishButton = view.findViewById(R.id.btnFinish)
+        finishButton?.setOnClickListener {
+            onCloseButtonClick()
+        }
+
+        return view
+    }
+
+    private fun onCloseButtonClick(){
+        activity?.finish()
     }
 
     companion object {
